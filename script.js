dbaf39b613fb3f6fe52fdb9c93eaf898e11c8e10
Luca <wachters.luca@gmail.com>
2021-02-25 23:36:28 +0100

    creating a playing field

--- script.js
+++ script.js
@@ -15,4 +15,35 @@ function applySettings(settings){
     settings.solidWalls = document.getElementById("i-solid").checked;
 
     console.log(settings);
+    fieldLoad(document.getElementById(`field`), GameSettings)
+
+}
+function fieldLoad(field, settings = {}){
+    var size = 20
+    console.log("loading");
+    console.log(field);
+    style = field.style;
+    style.width = `${size * settings.width}px`;
+    style.height = `${size*settings.height}px`;
+    if (settings.solidWalls) {
+        style.border = `double white 6px`
+    }
+    else{
+        style.border = `none`
+    }
+}
+function updateMaxSize() {
+    console.log("oui");
+    var width = document.getElementById("i-width");
+    var height = document.getElementById("i-height");
+    var game = document.getElementById("game");
+    width.max = Math.floor((game.offsetWidth - 40) / 20);
+    height.max = Math.floor((game.offsetHeight - 40) / 20);
+}
+function onLoad() {
+    fieldLoad(document.getElementById(`field`), GameSettings);
+    updateMaxSize()
+}
+function onResize() {
+    updateMaxSize()
 }
